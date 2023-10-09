967daee9834715df8cdf4361d732265edb16de97
twangpodorji <02230311.cst@rub.edu.bt>
2023-10-09 23:15:03 +0600

    task1

--- task1.py
+++ task1.py
@@ -1,1 +1,8 @@
-print(2)
+#Task 1
+#Display “Hello” as an output using print() function
+
+#Expected outcome:
+#When you run the Python file, “Hello” should be displayed without the quotation marks
+
+print('hi')
+
--- task2.py
+++ task2.py
@@ -1,1 +1,4 @@
-print("hi")
+#Task 2
+#Display “Hi how are you?” as an output using print() function
+#Expected outcome:
+#When you run the Python file, “Hi how are you?” should be displayed without the quotation marks
