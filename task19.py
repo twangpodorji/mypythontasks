e9621fb6cd8487631baf0180ac357eee73facaea
twangpodorji <02230311.cst@rub.edu.bt>
2023-10-10 22:26:21 +0600

    task

---
+++ task19.py
@@ -0,0 +1,22 @@
+#Task 19
+#Get input from the user by saying “Enter your name: ”
+#Store the user input to a variable called userName
+#Get input from the user by saying “Enter your age: ”
+#Store the user input to a variable called userAge
+#In the output, display as: “Hi <USERNAME>, your age is <USERAGE>”
+
+#Expected outcome:
+#When the code runs, the terminal will wait for an input with a text saying “Enter your name: ”
+#When you type in the input and press enter
+#The terminal will again wait for an input with a text saying “Enter your age: ”
+#When you type in the input and press enter
+#The output will be , “Hi <USERNAME>, your age is <USERAGE>”
+#The program stops
+#Combine useInput, variable & print together
+userName = input("Enter your name:")
+userAge = input("Enter your age:")
+#print("Hi " + userName + ", your age is " + userAge)
+
+print("hi" + userName + " ,your age is" + userAge)  # other way to achive it : print(f"Hi {userName}, your age is {userAge}")
+
+ 
